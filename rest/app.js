6fdaceaf0ab1066bcd1b71eef008f801a80955ce
Filmon-coder <eaglefifi@gmail.com>
2021-02-26 18:38:48 -0500

    Amazon Project stage 2

---
+++ rest/app.js
@@ -0,0 +1,33 @@
+const productData = [{
+    img: "th (1).jpg",
+    title: "shirt",
+    price: "$100",
+    button: "Add to cart"
+
+}]
+
+const container = document.getElementById('grid');
+
+productData.forEach((result) => {
+
+    const card = document.createElement('div');
+
+    const content = `
+    <div class="card">
+
+       <img src=${result.img} class="card-img-top" alt="HOODIE">
+
+       <div class="card-body">
+
+         <h5 class="card-title">${result.title}</h5>
+         <p class="card-text">${result.price}</p>
+
+         <div class="d-grid gap-2 col- mx-auto">
+           <button type="button" class="btn btn-secondary btn-sm" id="buy">${result.button}</button>
+         </div>
+         
+       </div>
+    </div>`; 
+
+    container.innerHTML += content;
+})
